cb3bac481f3d01b750b58c1712fa1daff18f6e24
Marvin ROGER <dev@marvinroger.fr>
2019-02-12 00:26:17 +0100

    :label: add types

---
+++ src/index.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Get the ESLint configuration object
+ */
+declare function getEslintConfig(): any
+
+/**
+ * Get the Jest configuration object
+ */
+declare function getJestConfig(): any
+
+/**
+ * Get the Prettier configuration object
+ */
+declare function getPrettierConfig(): any
