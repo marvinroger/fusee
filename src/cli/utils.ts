cfd7f7471220ab16b675c9e0a87adecdf0e1ce04
Marvin ROGER <dev@marvinroger.fr>
2019-10-11 14:02:56 +0200

    fix: use local bin instead of execa

--- src/cli/utils.ts
+++ src/cli/utils.ts
@@ -1,4 +1,8 @@
 import execa from 'execa'
+import * as path from 'path'
+
+const LOCAL_BIN_PATH = path.resolve(__dirname, '../../node_modules/.bin')
+const IS_WINDOWS = process.platform === 'win32'
 
 export function die(msg: string): void {
   process.exitCode = 1
@@ -9,9 +13,8 @@ export async function runLocalBin(
   command: string,
   args: string[] = []
 ): Promise<void> {
-  await execa(command, args, {
-    preferLocal: true,
-    localDir: __dirname,
+  const resolvedCommand = `${command}${IS_WINDOWS ? '.cmd' : ''}`
+  await execa(path.resolve(path.join(LOCAL_BIN_PATH, resolvedCommand)), args, {
     stdio: 'inherit',
   })
 }
