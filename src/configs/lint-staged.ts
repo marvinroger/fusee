20964bba700dadc526c39ffdd477ade5ea9a77e0
Marvin ROGER <bonjour+git@marvinroger.fr>
2020-03-04 11:37:15 +0100

    chore: upgrade dependencies

--- src/configs/lint-staged.ts
+++ src/configs/lint-staged.ts
@@ -2,7 +2,7 @@ import { LINT_GLOB } from '../constants'
 import { FuseeParams } from '../fusee'
 
 const makeConfig = (_fuseeParams: FuseeParams) => ({
-  [LINT_GLOB]: [`fusee lint`, 'git add'],
+  [LINT_GLOB]: [`fusee lint`],
 })
 
 export function buildGetLintStagedConfig(fuseeParams: FuseeParams) {
